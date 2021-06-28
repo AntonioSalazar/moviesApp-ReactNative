ad15e14075ec8cad905785ecb5e50fcbaeb755f2
unknown <antonio.salazar.emrsn@gmail.com>
2021-06-27 23:54:53 -0500

    testing the Animation for fading in and out

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import Navigation from './src/navigation/Navigation';
+import FadeScreen from './src/screens/FadeScreen';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigation />
+      {/* <Navigation /> */}
+      <FadeScreen />
     </NavigationContainer>
   );
 };
